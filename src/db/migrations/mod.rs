bda7dc572885627368331c4e276e79bbcf32530e
Erdem <zahrakon06@gmail.com>
2026-01-09 20:36:31 +0300

    Format migration names

--- src/db/migrations/mod.rs
+++ src/db/migrations/mod.rs
@@ -1,8 +1,8 @@
 use sea_orm_migration::prelude::*;
 
 pub mod m000001_create_guild_table;
-pub mod m20240109_000001_add_log_channel;
-pub mod m20240109_000002_create_module_config;
+pub mod m000002_add_log_channel;
+pub mod m000003_create_module_config;
 
 pub struct Migrator;
 
@@ -11,8 +11,8 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m000001_create_guild_table::Migration),
-            Box::new(m20240109_000001_add_log_channel::Migration),
-            Box::new(m20240109_000002_create_module_config::Migration),
+            Box::new(m000002_add_log_channel::Migration),
+            Box::new(m000003_create_module_config::Migration),
         ]
     }
 }
